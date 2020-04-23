55bbe1007da8ea52aca8e356ddf8d05942f6524c
Mik Pedersen <45688364+MikPedersen@users.noreply.github.com>
2020-04-23 14:23:35 +0200

    Alot of refactors done to fit our wording

--- app/src/main/java/com/datamatiker/bingeflix/ui/LikeDislike/LikeDislikeFragment.java
+++ app/src/main/java/com/datamatiker/bingeflix/ui/LikeDislike/LikeDislikeFragment.java
@@ -1,4 +1,4 @@
-package com.datamatiker.bingeflix.ui.gallery;
+package com.datamatiker.bingeflix.ui.LikeDislike;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -12,17 +12,17 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import com.datamatiker.bingeflix.R;
 
-public class GalleryFragment extends Fragment {
+public class LikeDislikeFragment extends Fragment {
 
-    private GalleryViewModel galleryViewModel;
+    private LikeDislikeViewModel likeDislikeViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
-                ViewModelProviders.of(this).get(GalleryViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
-        galleryViewModel.getText().observe(this, new Observer<String>() {
+        likeDislikeViewModel =
+                ViewModelProviders.of(this).get(LikeDislikeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_likedislike, container, false);
+        final TextView textView = root.findViewById(R.id.text_likeDislike);
+        likeDislikeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
