f2cc3763401e2954f01abda3b4b6374f5f04f08d
Emil Nymann <emilnymann96@gmail.com>
2020-04-25 15:42:31 +0200

    Added Application class to initialize AndroidNetworking library

---
+++ app/src/main/java/BingeflixApplication.java
@@ -0,0 +1,10 @@
+import android.app.Application;
+import com.androidnetworking.AndroidNetworking;
+
+public class BingeflixApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AndroidNetworking.initialize(getApplicationContext());
+    }
+}
